3a1a6cac2eea7d5abf2b1f194b8455a6e4a70964
jali-k <e19210@eng.pdn.ac.lk>
2023-09-11 14:06:10 +0530

    Send POST requests to the firebase backend

--- lib/screens/new_Item_screen.dart
+++ lib/screens/new_Item_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -25,11 +27,19 @@ class _NewItemScreenState extends State<NewItemScreen> {
     void _saveItem() {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        Navigator.of(context).pop(GroceryItem(
-            id: DateTime.now().toString(),
-            name: _enteredName,
-            quantity: _enteredQuantity,
-            category: _selectedCategory!));
+        final url = Uri.https(
+            'flutter-shop-app-cc6dc-default-rtdb.firebaseio.com',
+            'shopping-list.json');
+        http.post(url,
+            headers: {
+              'Content-Type':
+                  'application/json' //Tells that this obj is in json format
+            },
+            body: json.encode({
+              'name': _enteredName,
+              'quantity': _enteredQuantity,
+              'category': _selectedCategory!.name
+            })); // encode the body as a json obj
       }
     }
 
